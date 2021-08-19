16aa605b21942d698fae7ec39e48fceffa78a797
André Sterba <git@sterba.dev>
2021-08-19 15:08:53 +0200

    Fix lint errors

--- api/src/router/constants-router.ts
+++ api/src/router/constants-router.ts
@@ -3,11 +3,5 @@ import { ConstantsController } from "../controller/constants-controller";
 
 export const constantsRouter: Router = Router({ mergeParams: true });
 
-constantsRouter.get(
-    "/",
-    ConstantsController.getAllConstants
-);
-constantsRouter.patch(
-    "/",
-    ConstantsController.updateConstants
-);
+constantsRouter.get("/", ConstantsController.getAllConstants);
+constantsRouter.patch("/", ConstantsController.updateConstants);
