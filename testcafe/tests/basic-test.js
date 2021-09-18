46b61f40440f3644f4af4fd567ef407ff6bef725
Neil Schark <github@schark.eu>
2021-09-18 22:04:25 +0200

    wip

--- testcafe/tests/basic-test.js
+++ testcafe/tests/basic-test.js
@@ -2,7 +2,7 @@
 import { Selector } from 'testcafe';
 
 fixture `Getting Started`
-    .page `http://127.0.0.1:8080/`;
+    .page `http://localhost:8080/`;
 
 test('My first test', async t => {
     // Test code
