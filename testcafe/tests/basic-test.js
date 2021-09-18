f2b0464a84524178b65ed5f0c35278dd01280fb8
Neil Schark <github@schark.eu>
2021-09-18 23:18:44 +0200

    basic test

--- testcafe/tests/basic-test.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-import { Selector } from 'testcafe';
-
-fixture `Getting Started`
-    .page `http://127.0.0.1:8080/`;
-
-test('My first test', async t => {
-    // Test code
-});
