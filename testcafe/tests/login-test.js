82e97a4eafaf3bdd76413be4484622b457830bdb
Neil Schark <github@schark.eu>
2021-09-18 23:26:48 +0200

    wip

--- testcafe/tests/login-test.js
+++ testcafe/tests/login-test.js
@@ -4,6 +4,7 @@ import { Selector } from 'testcafe';
 fixture `Login Test`
     .page `http://127.0.0.1:8080/`;
 
+//Unfinished test, just as an example to test the ci
 test('Login', async t => {
     await t
         .typeText('#input-18', 'Admin')
