c5ad302bbaf8325f6fea9ab3409702c02c1f0e3d
André Sterba <git@sterba.dev>
2021-10-03 12:56:40 +0200

    Increase size of username

--- app/src/components/user-page/user-page.vue
+++ app/src/components/user-page/user-page.vue
@@ -5,7 +5,7 @@
                 <v-list-item-content>
                     <v-list-item-title >
                         <span class="headline mb-1">  Username: </span>
-                        {{ user.name }}
+                        <span class="headline mb-1"> {{ user.name }} </span>
                     </v-list-item-title>
                 </v-list-item-content>
             </v-list-item>
